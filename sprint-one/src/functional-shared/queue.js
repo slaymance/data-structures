72438ee758af63ddb64ff98d997d139955838d9e
Shane Laymance <shane.laymance@gmail.com>
2017-08-23 15:16:07 -0700

    Prime queue results

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,6 +1,12 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queue = {
+    storage: {},
+    count: 0
+  };
+  _.extends(queue, queueMethods);
+  return queue;
 };
 
 var queueMethods = {};

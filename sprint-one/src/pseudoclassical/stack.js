40504a11e1815a6a990c47c18cf4a2e07db360ca
Shane Laymance <shane.laymance@gmail.com>
2017-08-23 16:38:52 -0700

    Complete mvp pseudoclassical stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,22 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.count = 0;
+};
+
+Stack.prototype.size = function() {
+  return this.count;
+};
+Stack.prototype.push = function(value) {
+  this.storage[this.count] = value;
+  this.count++;
+};
+Stack.prototype.pop = function() {
+  var popped = this.storage[this.count - 1];
+  delete this.storage[this.count - 1];
+  this.count > 0 ? this.count-- : null;
+  return popped;
 };
 
 

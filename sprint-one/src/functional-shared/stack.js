13d540d9e982adf8ff8f227e81dc61560d9d13d3
Shane Laymance <shane.laymance@gmail.com>
2017-08-23 15:05:32 -0700

    Prime class function with shared methods

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,13 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stack = {};
+  stack.storage = {};
+  stack.count = 0;
+
+  _.extend(stack, stackMethods);
+
+  return stack;
 };
 
 var stackMethods = {};
-
-

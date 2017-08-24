f48e79c541eaf88dc66f6e239072b492b1f6d453
Shane Laymance <shane.laymance@gmail.com>
2017-08-24 11:20:12 -0700

    Complete linked list addToTail, removeHead, and contains methods

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,52 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    //create new Node
+    //assign tail to node
+    //condition: if the head is null, add tail to head
+    //else if head is pointing to null 
+      //then we point the head's next to the tail
+    var node = Node(value);
+    var previousTail = list.tail;
+    list.tail = node;
+    
+
+    if (list.head === null) {
+      list.head = node;
+    } else if (list.head.next === null) {
+      list.head.next = list.tail;
+    } else {
+      previousTail.next = node;
+    }
   };
 
   list.removeHead = function() {
+    //assign current head value to variable, to return later
+    //assign head to head.next
+    var headValue = list.head.value;
+    list.head = list.head.next;
+    return headValue;
   };
 
   list.contains = function(target) {
+    // create a function to recursively check each node's value to find target
+      // if node.value === target;
+      //   return true;
+      // else if node.next !== null 
+      //   return itself with node.next;
+      // else return false
+
+    // return our recursiveFunction(list.head);
+    var checkNode = function(node) {
+      if (node.value === target) {
+        return true;
+      } else if (node.next !== null) {
+        return checkNode(node.next);
+      } else {
+        return false;
+      }
+    };
+    return checkNode(list.head);    
   };
 
   return list;

95b827a36910c3d66d876154b62a24ced06c7d50
Shane Laymance <shane.laymance@gmail.com>
2017-08-26 10:50:06 -0700

    Implement hash resize

--- sprint-advanced-content/src/hashTable.js
+++ sprint-advanced-content/src/hashTable.js
@@ -1,11 +1,15 @@
-
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this.size = 0;
 };
 
 HashTable.prototype.insert = function(k, v) {
+  this.size++;
+  if (this.size >= this._limit * 0.75) {
+    this.resize('double');
+  }
+
   var index = getIndexBelowMaxForKey(k, this._limit);
 
   if (this._storage.get(index) === undefined) {
@@ -19,7 +23,8 @@ HashTable.prototype.insert = function(k, v) {
       replaceKey = true;
       break;
     }
-  } if (!replaceKey) {
+  } 
+  if (!replaceKey) {
     indexArray.push([k, v]);
   }
   this._storage.set(index, indexArray);
@@ -37,6 +42,14 @@ HashTable.prototype.retrieve = function(k) {
 };
 
 HashTable.prototype.remove = function(k) {
+  if (this.size > 0) {
+    this.size--;
+  }
+  if (this.size <= this._limit * 0.25 && this._limit > 8) {
+    //half size before delete [k,v]
+    this.resize('half');
+  }
+
   var index = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(index);
   for (var i = 0; i < bucket.length; i++) {
@@ -48,6 +61,27 @@ HashTable.prototype.remove = function(k) {
   this._storage.set(index, bucket);
 };
 
+HashTable.prototype.resize = function(indicator) {
+  var factor;
+  if (indicator === 'double') {
+    factor = 2;
+  } else {
+    factor = 0.5;
+  }
+
+  var oldStorage = this._storage;
+  this._limit *= factor;
+  this._storage = LimitedArray(this._limit);
+  this.size = 0;
+
+  oldStorage.each(bucket => {
+    if (bucket) {
+      for (var i = 0; i < bucket.length; i++) {
+        this.insert(bucket[i][0], bucket[i][1]);
+      }
+    }  
+  });
+};
 
 
 /*
